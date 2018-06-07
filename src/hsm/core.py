4bdcc06690b6230eafb994ea98071fb97a0caa83
Florian Patzelt <fpatzelt@techfak.uni-bielefeld.de>
2018-06-07 11:34:18 +0200

    Added method to run statemachine with eventqueue

--- src/hsm/core.py
+++ src/hsm/core.py
@@ -19,6 +19,7 @@ from six import iteritems
 import collections
 from collections import deque
 from threading import Thread
+import Queue
 
 import logging
 _LOGGER = logging.getLogger("hsm.core")
@@ -817,3 +818,25 @@ class CallbackState(ProcessingState):
 
     def execute(self, event = None):
         return self._cb(event, *self._args, **self._kwargs)
+
+def run(sm, final_state):
+    """Run the statemachine until the final state is reached. Events are dispatched a synchronous way.
+    """
+    event_queue = Queue.Queue()
+    def dispatch_with_queue(event):
+        event_queue.put(event)
+
+    sm._dispatch = sm.dispatch
+    sm.dispatch = dispatch_with_queue
+
+    sm.initialize()
+    def finished():
+        return sm.leaf_state == final_state or sm.leaf_state.is_substate(final_state)
+
+    while not finished():
+        event = event_queue.get()
+        sm._dispatch(event)
+
+    print('finished')
+    sm.dispatch = sm._dispatch
+    del(sm._dispatch)
